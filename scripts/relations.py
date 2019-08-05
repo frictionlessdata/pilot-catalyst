8244efa24e7eb564d365a6c9d10f68ce7d59b787
roll <eskarev@gmail.com>
2019-08-05 14:03:35 +0300

    Added scripts

---
+++ scripts/relations.py
@@ -0,0 +1,12 @@
+from datapackage import Package
+from datapackage.exceptions import RelationError
+
+
+# Check relations
+
+package = Package('data/ferc1-test/datapackage.json')
+try:
+    package.get_resource('fuel_ferc1').check_relations()
+    print('Relations are checked')
+except RelationError as exception:
+    print(exception)
